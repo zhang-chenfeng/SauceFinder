240088155db099f5b09e40ca36f14b7497712c4c
ChenFeng <z.chenfengzhang@gmail.com>
2020-01-31 14:39:43 -0500

    ui doesn't look like crap now if no info is loaded

--- sauce.py
+++ sauce.py
@@ -39,7 +39,7 @@ class MainUI(tk.Frame):
         self.img_tmp = ImageTk.PhotoImage(Image.open("template.png"))
         
         # header
-        self.header = tk.Label(self.root, text="Sauce Finder", font=(None, 15))
+        self.header = tk.Label(self.root, width=80, text="Sauce Finder", font=(None, 15))
         
         # input frame
         self.input_f = tk.Frame(self.root)
@@ -48,27 +48,27 @@ class MainUI(tk.Frame):
         self.search = tk.Button(self.input_f, text="GO", command=self.fetchSauce)
         
         # sub headers
-        self.title_l = tk.Label(self.root, text="", font=(None, 14))
-        self.subtitle_l = tk.Label(self.root, text="", font=(None, 12))
+        self.title_l = tk.Label(self.root, text=" ", font=(None, 14), wraplength=875)
+        self.subtitle_l = tk.Label(self.root, text=" ", font=(None, 12), wraplength=875)
         
         # preview frame
         self.preview_f = tk.Frame(self.root)
         self.cover_l = tk.Label(self.preview_f, image=self.img_tmp)
         self.fields_f = tk.Frame(self.preview_f)
         
-        self.header.grid(row=0, column=0, columnspan=5, padx=(30, 30), pady=(15, 10))
+        self.header.grid(row=0, column=0, padx=(30, 30), pady=(15, 10))
         
-        self.input_f.grid()
+        self.input_f.grid(row=1, column=0)
         self.prompt.grid(row=0, column=0, padx=(5, 2))
         self.entry.grid(row=0, column=1, padx=(2, 2))
         self.search.grid(row=0, column=2, padx=(2, 5))
         
-        self.title_l.grid(row=2, column=0, columnspan=5, padx=(30, 30), pady=(15, 2))
-        self.subtitle_l.grid(row=3, column=0, columnspan=5, padx=(0, 0), pady=(5, 10))
+        self.title_l.grid(row=2, column=0, padx=(30, 30), pady=(15, 2), sticky=tk.E+tk.W)
+        self.subtitle_l.grid(row=3, column=0, padx=(30, 30), pady=(5, 10), sticky=tk.E+tk.W)
 
-        self.preview_f.grid(row=4, pady=(5, 10))
-        self.cover_l.grid(row=0, column=0, padx=(30, 30), sticky=tk.N)        
-        self.fields_f.grid(row=0, column=1, sticky=tk.N)
+        self.preview_f.grid(row=4, padx=(30, 30), pady=(5, 15), sticky=tk.W+tk.E)
+        self.cover_l.grid(row=0, column=0, padx=(10, 10), sticky=tk.N+tk.W)        
+        self.fields_f.grid(row=0, column=1, padx=(0, 10), sticky=tk.N)
         
         
     def renderPreview(self):
@@ -79,7 +79,7 @@ class MainUI(tk.Frame):
         # page count and upload date are rendered same as fields in this view
         fields.append(("Pages", [str(pages)]))
         fields.append(("Uploaded", [upload_time]))
-        
+
         self.title_l['text'] = title
         self.subtitle_l['text'] = subtitle
         
@@ -90,7 +90,7 @@ class MainUI(tk.Frame):
         # render fields & tags
         for index, (field, tags) in enumerate(fields):
             tk.Label(self.fields_f, text=field, font=(None, 12)).grid(row=index, column=0, sticky=tk.E+tk.N)
-            tk.Label(self.fields_f, text=", ".join(tags), font=(None, 11), wraplength=450, justify='left').grid(row=index, column=1, sticky=tk.W+tk.N, padx=(10, 10), pady=(0, 20))       
+            tk.Label(self.fields_f, text=", ".join(tags), font=(None, 12), wraplength=440, justify='left').grid(row=index, column=1, sticky=tk.W+tk.N, padx=(10, 0), pady=(0, 20))       
 
     # Future loading events go in here
     def loadDisplay(self, magic_number):
