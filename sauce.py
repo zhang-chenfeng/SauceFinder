575dafcd2f354813f3b4d7efd23fe872b4b2c464
ChenFeng <z.chenfengzhang@gmail.com>
2020-02-12 14:39:59 -0500

    fixes so both view types run stable

--- sauce.py
+++ sauce.py
@@ -58,6 +58,7 @@ class MainUI(tk.Frame):
         self.prompt = tk.Label(self.input_f, text="Enter Sauce:")
         self.entry = tk.Entry(self.input_f, width=10)
         self.entry.bind("<FocusIn>", lambda event: self.entry.selection_range(0, tk.END))
+        self.entry.bind("<Return>", lambda event: self.fetchSauce())
         self.search = tk.Button(self.input_f, text="GO", command=self.fetchSauce)
         
         # settings
@@ -260,22 +261,29 @@ class Viewer(tk.Toplevel):
         tk.Toplevel.__init__(self, base)
         self.root = base.root
         self.base = base
+        
         self.pages = self.base.sauce_data[4]
         self.gallery = self.base.sauce_data[6]
-        self.curr_page = 1
+        
+        self.img_w, self.img_h = self.base.memory[1].size
         self.win_h = self.base.height - 32
+        self.xpad = (20, 20)
+        self.ypad = (10, 30)
+        
+        self.curr_page = 1
+                
         self.pressed = False
         self.loading = False
+        
         self.q = queue.Queue()
-        self.xpad, self.ypad = 20, 20
         
-        self.views = {'scaled': Scale, 'scrolled': Scroll}
         self.UI()
+        self.views = {'scaled': Scale, 'scrolled': Scroll}
         self.viewframe = self.views[self.base.viewmode](self)
-        print(self.base.memory)
+        self.viewframe.pack(padx=self.xpad, pady=self.ypad)
         self.loadPage()
-        
-    
+
+
     def UI(self):
         self.transient(self.base) # popup appears as part of main window(not shown on taskbar)
         self.focus() # give keyboard focus to the toplevel object(for key bindings)
@@ -291,39 +299,6 @@ class Viewer(tk.Toplevel):
         self.bind('<KeyRelease-Left>', self.resetPress)
         self.bind('<KeyRelease-Right>', self.resetPress)
 
-    
-    def scrollView(self):
-        win_h = self.base.height - 32
-        self.img_h = win_h - 2 * self.ypad
-        
-        self.screen = tk.Canvas(self.display_f, width=1080, height=win_h-2*self.ypad)
-        self.bar = tk.Scrollbar(self.display_f, orient='vertical', command=self.screen.yview)
-        self.screen.configure(yscrollcommand=self.bar.set)
-        
-        self.screen.grid(row=0, column=0)
-        self.bar.grid(row=0, column=1, sticky='ns')
-        
-        self.bind("<MouseWheel>", self.scroll)
-
-
-    def scrollRender(self):
-        self.img_display = ImageTk.PhotoImage(self.base.memory[self.curr_page])
-        self.screen.create_image(0, 0, image=self.img_display, anchor='nw')
-        self.screen.configure(scrollregion=self.screen.bbox('all'))
-        self.screen.yview_moveto(0)
-        
-
-    def scroll(self, event):
-        if event.state == 0:
-            self.screen.yview_scroll(int(-1 * (event.delta / 120)), 'units')
-    
-    
-    def renderPage(self):
-        print("rendering page {}".format(self.curr_page))
-        self.viewframe.render(self.base.memory[self.curr_page])
-        print("buffering page {}".format(self.curr_page + 1))
-        self.bufferNext()
-
 
     def loadPage(self):
         print("call to load page {}".format(self.curr_page))
@@ -331,29 +306,20 @@ class Viewer(tk.Toplevel):
             self.renderPage()
 
         except KeyError: # Image is still loading need to wait
+            self.loading = True
             print("load not finished retry in 100ms")
             self.root.after(100, self.loadPage)
 
 
-    def loadPageold(self):
-        self.loading = True
-        try:
-            image = self.base.memory[self.curr_page]
-            print("Already loaded")
-            self.q.put(0)
-        except KeyError:
-            # offline testing
-            if self.base.magic_number == "test":
-                self.base.memory[self.curr_page] = Image.open("u" + str(self.curr_page) + ".png")
-                print("load testing image")
-                self.q.put(0)
-            
-            else:
-                print("image download")
-                Thread(target=self.downloadImage, args=(self.gallery, self.curr_page, self.q, self.base.memory)).start()
-        self.waitImage()
-        
-        
+    def renderPage(self):
+        print("rendering page {}".format(self.curr_page))
+        self.viewframe.render(self.base.memory[self.curr_page])
+        self.title("".join((self.base.magic_number, "- page ", str(self.curr_page))))
+
+        print("buffering page {}".format(self.curr_page + 1))
+        self.bufferNext()
+
+
     def bufferNext(self):
         if self.curr_page < self.pages:
             try:
@@ -407,11 +373,6 @@ class Viewer(tk.Toplevel):
 
     def resetPress(self, event):
         self.pressed = False
-        
-    
-    # testing alternative ui
-    def altUI(self):
-        self.canvas = tk.Canvas(self)
 
 
 
@@ -419,23 +380,24 @@ class Scale(tk.Frame):
     def __init__(self, base):
         tk.Frame.__init__(self, base)
         self.base = base
-        self.img_h = self.base.win_h - 2 * self.base.ypad
-        self.img_w = int(self.img_h * 0.6968)
-        win_w = self.img_w + 2 * self.base.xpad
+        ratio = self.base.img_w / self.base.img_h
+
+        self.scaled_height = self.base.win_h - sum(self.base.ypad)
+        self.scaled_width = int(self.scaled_height * ratio)
+        win_w = self.scaled_width + sum(self.base.xpad)
         
-        self.base.geometry("{}x{}+{}+0".format(self.img_w, self.base.win_h, (self.base.base.width - self.img_w) // 2))
+        self.base.geometry("{}x{}+{}+0".format(win_w, self.base.win_h, (self.base.base.width - win_w) // 2))
         self.base.update_idletasks() # required for whatever reason
 
         self.img_l = tk.Label(self)
         self.img_l.grid()
-        
-        self.pack()
+
         self.img_l = tk.Label(self)
         self.img_l.grid()
     
     
     def render(self, image):
-        self.img_display = ImageTk.PhotoImage(image.resize((self.img_w, self.img_h), Image.ANTIALIAS))
+        self.img_display = ImageTk.PhotoImage(image.resize((self.scaled_width, self.scaled_height), Image.ANTIALIAS))
         self.img_l['image'] = self.img_display
 
 
@@ -443,8 +405,34 @@ class Scale(tk.Frame):
 class Scroll(tk.Frame):
     def __init__(self, base):
         tk.Frame.__init__(self, base)
+        self.base = base
+        
+        win_w = self.base.img_w + sum(self.base.xpad)
+        screen_height = self.base.win_h - sum(self.base.ypad)
+        
+        self.base.geometry("{}x{}+{}+0".format(win_w, self.base.win_h, (self.base.base.width - win_w) // 2))
+        self.base.update_idletasks()
+        
+        self.screen = tk.Canvas(self, width=self.base.img_w, height=screen_height)
+        self.bar = tk.Scrollbar(self, orient='vertical', command=self.screen.yview)
+        self.screen.configure(yscrollcommand=self.bar.set)
+        
+        self.screen.grid(row=0, column=0)
+        self.bar.grid(row=0, column=1, sticky='ns')
+        
+        self.base.bind("<MouseWheel>", self.scroll)
+    
 
-
+    def render(self, image):
+        self.img_display = ImageTk.PhotoImage(image)
+        self.screen.create_image(0, 0, image=self.img_display, anchor='nw')
+        self.screen.configure(scrollregion=self.screen.bbox('all'))
+        self.screen.yview_moveto(0)
+        
+        
+    def scroll(self, event):
+        if event.state == 0:
+            self.screen.yview_scroll(int(-1 * (event.delta / 120)), 'units')
 
 
 
